1f40f2205a235fb7b5f413b3b186cb79b2525a3c
asubbaswamy-fda <adarsh.subbaswamy@fda.hhs.gov>
2024-01-19 15:12:03 -0500

    Reduce number of R packages to install

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,5 +1,5 @@
-load("R_packages.rda")
-
+# load("R_packages.rda")
+installedpackages = c('argparse', 'sirus')
 for (count in 1:length(installedpackages)) {
-    install.packages(installedpackages[count])
+    install.packages(installedpackages[count],repos="http://cran.us.r-project.org")
 }
